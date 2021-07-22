7ff68d37f3f55d5f9dc4ee48d51b640055316e3e
Anton <samantonovich@gmail.com>
2021-07-22 16:56:47 +0300

    HW_11: Add task 1 proj

---
+++ HM_11/Task1/Models/Song.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task1.Models
+{
+    enum Genre
+    {
+        Other,
+        Classic,
+        Rock,
+        Jazz,
+        Pop,
+        HipHop
+    }
+    class Song
+    {
+        public string Name { get; set; }
+        public int Duration { get; set; }
+        public string Author { get; set; }
+        public DateTime CreationDate { get; set; }
+        public Genre Genre { get; set; }
+
+        public Song(string name, int duration, string author, DateTime creationDate, Genre genre) =>
+            (Name, Duration, Author, CreationDate, Genre) = (name, duration, author, creationDate, genre);
+
+        public Song(string name, int duration, string author, DateTime creationDate, int genre) :
+            this(name, duration, author, creationDate, Enum.IsDefined(typeof(Genre), genre) ? (Genre)genre : default(Genre))
+        { }
+    }
+}
---
+++ HM_11/Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Task1.Models;
+using Task1.Services;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ISongService songService = new SongService();
+            var song = new Song("New Divide", 3, "Linkin Park", new DateTime(2009, 05, 18), Genre.Rock);
+            Console.WriteLine($"Serialized object:\n{songService.GetSongData(song)}");
+
+            var serializeCollection = Enumerable.Repeat(songService.GetSongData(song), 100000);
+            //Newtonsoft speedtest
+            SpeedTest(new NewtonsoftService(), serializeCollection);
+            //Text.Json speedtest
+            SpeedTest(new TextJsonService(), serializeCollection);
+        }
+
+        private static void SpeedTest(ISerializeService serializeService, object serializeObj)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            serializeService.DeserializeObject(serializeService.SerializeObject(serializeObj));
+            sw.Stop();
+            Console.WriteLine($"Ellapsed ms: {sw.ElapsedMilliseconds}");
+        }
+    }
+}
