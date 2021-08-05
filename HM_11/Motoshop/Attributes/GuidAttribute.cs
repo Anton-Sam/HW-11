a9937420c0b667b655af7be2529758a9cc31ed04
Anton <samantonovich@gmail.com>
2021-08-05 19:41:01 +0300

    HW15: Add Motoshop

---
+++ HM_11/Motoshop/Attributes/GuidAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Motoshop.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class GuidAttribute : DataTypeAttribute
+    {     
+        public GuidAttribute() : base("Guid")
+        {
+            ErrorMessage = "Value is not Guid type";
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+                return false;
+            
+            return Guid.TryParse(value.ToString(), out _);
+        }
+    }
+}
---
+++ HM_11/Motoshop/Attributes/MinYearAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Motoshop.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class MinYearAttribute : DataTypeAttribute
+    {
+        public int MinYear { get; set; }
+
+        public MinYearAttribute() : base("DateTime")
+        {           
+        }
+
+        public override bool IsValid(object value)
+        {
+            ErrorMessage = $"Value is must be within [{MinYear} year; {DateTime.Now.ToShortDateString()}]";
+            if (value is not DateTime)
+                return false;
+            var dateTime = (DateTime)value;
+            return dateTime.Year >= MinYear && dateTime <= DateTime.Now; 
+        }
+    }
+}
